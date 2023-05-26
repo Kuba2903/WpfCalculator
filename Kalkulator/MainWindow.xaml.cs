28fea3f0f50666e70ac595aca6853e9003d3e5b3
Kuba2903 <jakub.adamczyk@microsoft.wsei.edu.pl>
2023-05-26 19:29:52 +0200

    second commit

--- Kalkulator/MainWindow.xaml.cs
+++ Kalkulator/MainWindow.xaml.cs
@@ -25,6 +25,7 @@ namespace Kalkulator
             InitializeComponent();
         }
 
+        public int I { get; set; } = 0;
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             Button button = (Button)sender;
@@ -34,25 +35,92 @@ namespace Kalkulator
 
         private void Button_ClickAdd(object sender, RoutedEventArgs e)
         {
-            int temp = int.Parse( numbers.Text);
-            result.Text += temp;
-
+            if (result.Text.Length > 0)
+                numbers.Text = result.Text;
+            else
+            {
+                double temp = double.Parse(numbers.Text);
+                result.Text += temp;
+            }
             numbers.Text = string.Empty;
+
+            Button button = (Button)sender;
+            switch (button.Content)
+            {
+                case "+":
+                    I = 1;
+                    break;
+                case "-":
+                    I = 2;
+                    break;
+                case "*":
+                    I = 3;
+                    break;
+                case "/":
+                    I = 4;
+                    break;
+            }
         }
 
         private void Button_ClickEquals(object sender, RoutedEventArgs e)
         {
             try
             {
-                int equals = int.Parse(result.Text) + int.Parse(numbers.Text);
+
+                double equals = 0;
+
+                switch (I)
+                {
+                    case 1:
+                        equals = double.Parse(result.Text) + double.Parse(numbers.Text);
+                        break;
+                    case 2:
+                        equals = double.Parse(result.Text) - double.Parse(numbers.Text);
+                        break;
+                    case 3:
+                        equals = double.Parse(result.Text) * double.Parse(numbers.Text);
+                        break;
+                    case 4:
+                        equals = double.Parse(result.Text) / double.Parse(numbers.Text);
+                        break;
+                }
 
                 result.Text = equals.ToString();
 
                 numbers.Text = string.Empty;
-            }catch (Exception ex)
+            }
+            catch (Exception ex)
             {
 
             }
+        }
+
+        private void Button_ClickReset(object sender, RoutedEventArgs e)
+        {
+            I = 0;
+            numbers.Text = string.Empty;
+            result.Text = string.Empty;
+        }
+
+        private void Button_ClickDelete(object sender, RoutedEventArgs e)
+        {
+            if(numbers.Text.Length > 0)
+                numbers.Text = numbers.Text.Remove(numbers.Text.Length - 1);
+        }
+
+        private void Button_ClickSign(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                double num = double.Parse(numbers.Text);
+                num = num * -1;
+                numbers.Text = num.ToString();
+            }catch (Exception ex) { }
+        }
+
+        private void Button_Clickfloat(object sender, RoutedEventArgs e)
+        {
+            numbers.Text += ",";
         }
     }
 }
